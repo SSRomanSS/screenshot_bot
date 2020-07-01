8e39a83e94525f96b3639f2e9e087d9dbf1a6788
Roman Shkapiak <r.shkapiak@gmail.com>
2020-07-01 10:58:25 +0300

    Corrected height size detection with scrolling

--- screenshot.py
+++ screenshot.py
@@ -5,12 +5,12 @@ from selenium.common.exceptions import InvalidArgumentException
 
 
 class ScreenShot:
-    """Class for screenshot`s management."""
+    """Class for a screenshot`s management."""
     def __init__(self, url):
         self.url = url
 
     def fullpage_screenshot(self):
-        """Take screenshot."""
+        """Take a screenshot."""
         options = Options()
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--test-type')
@@ -20,24 +20,28 @@ class ScreenShot:
         options.add_argument('--disable-infobars')
         options.add_argument('--disable-extensions')
         driver = webdriver.Chrome(chrome_options=options)
+
+        def _scroll_size(size):
+            """Get window`s size after scrolling"""
+            return driver.execute_script('return document.body.parentNode.scroll' + size)
+
         try:
             driver.get(self.url)
         except InvalidArgumentException:
             driver.get(f'https://{self.url}')
 
-        total_height = driver.find_element("xpath", 'html').size["height"]
-        driver.set_window_size(1920, total_height)
+        driver.set_window_size(_scroll_size('Width'), _scroll_size(
+            'Height'))
+        driver.find_element_by_tag_name('body')
         driver.save_screenshot(f'{self.get_photo_name()}.png')
         driver.close()
 
     def get_photo_name(self):
-        """Create name for photo file."""
+        """Create a name for photo file."""
         return abs(hash(self.url))
 
     def delete_photo(self):
         """Delete photo file after sending"""
-        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), f'{self.get_photo_name()}.png')
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            f'{self.get_photo_name()}.png')
         os.remove(path)
-
-
-
